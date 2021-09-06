c38634c55f9c3139fa282b85db6c318a82e427ff
mbrie041 <mbrie041@gmail.com>
2021-09-06 14:25:00 -0400

    Complete assignment 3

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
+  visible = false;
+  clickArray = [];
+
+  clickedFunction() {
+    this.visible = !this.visible;
+    // this.clickArray.push(this.clickArray.length + 1);
+    this.clickArray.push(new Date());
+  }
 }
